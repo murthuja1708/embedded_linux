b719f848ff65cd870c6dc73befcad63bddbd3551
murthuja <murthuja1708@gmail.com>
2021-12-08 12:45:29 +0530

    updated code with 1 semaphore with value 4 instead of 4 semaphores with value 1

--- synchronization/semaphores/resource_mgmt.c
+++ synchronization/semaphores/resource_mgmt.c
@@ -5,11 +5,9 @@
 
 
 
-
-
-
 #define RESOURCE_COUNT   4
 #define THREAD_COUNT     10
+#define INITIAL_SEMA_VALUE RESOURCE_COUNT
 #define ROWS 4
 #define COLS 2
 
@@ -25,7 +23,7 @@ const int* start_address=resource[0];
 static int _resource_index=0;
 
 
-sem_t res_sema[4];
+sem_t res_sema;
 sem_t res_index_sem;
 
 
@@ -35,16 +33,17 @@ void* get_buffer()
     int* res;
 
     sem_wait(&res_index_sem);
+    _resource_index%=RESOURCE_COUNT;
     fprintf(stderr,"waiting to acquire  %d resource semaphore\n",_resource_index);
-    _resource_index%=4;
     
     //if 1 enters if 0 blocks and decrements the value
     
-    if(sem_wait(&res_sema[_resource_index]) == 0)
+    if(sem_wait(&res_sema) == 0)
     {
         fprintf(stderr,"acquired %d resource\n",_resource_index);
         res=(int*)start_address+(_resource_index*COLS);
         _resource_index++;
+        sem_post(&res_index_sem);
     }
     
     else
@@ -52,7 +51,6 @@ void* get_buffer()
         fprintf(stderr,"all resources are busy couldnt acquire\n");
         res=NULL;    
     }
-    sem_post(&res_index_sem);
     
     return res;
 }
@@ -63,62 +61,58 @@ void* release_buffer(void* addr)
     //printf("%ld array starts at %p and first value is %d\n",i,temp,temp[0]);
     int index=((int*)addr-start_address);
     index/=8;
-    if(index>=0 && index<4)
-    {
-        fprintf(stderr,"releasing  %d resource\n",index);
-        sem_post(&res_sema[index]);////increments the value and thread that blocked is woken up and locked again i..e decreased
-    }
+    fprintf(stderr,"releasing  %d resource\n",index);
+    sem_post(&res_sema);////increments the value and thread that blocked is woken up and locked again i..e decreased
 }
 
 int main(int argc,char* argv[])
 {
-    pthread_t resource_thread[10];
-    void* resrc;
+    pthread_t BufferTid[THREAD_COUNT];
+
 
     //initializing unnamed semaphores
 
-    unsigned int sem_value=1;
-
-
-    for (size_t i = 0; i < RESOURCE_COUNT; i++)
-    {
-        sem_init(&res_sema[i],0,sem_value);
-    }    
+    
+    sem_init(&res_sema,0,INITIAL_SEMA_VALUE);
     
     sem_init(&res_index_sem,0,1);
     //creating 10 threads
     
     for (size_t i = 0; i < THREAD_COUNT; i++)
     {
-        pthread_create(&resource_thread[i],NULL,get_buffer,NULL);
+        pthread_create(&BufferTid[i],NULL,get_buffer,NULL);
     }
 
     //creating int pointers to store the result
 
-    
-
     void* resource_ptr[RESOURCE_COUNT]={NULL};
     
     //joining the threads
+    
+    int sem_value;
 
     for (size_t i = 0; i < THREAD_COUNT; i++)
     {
         fprintf(stderr,"joining thread %ld \n",i);
-        pthread_join(resource_thread[i],&resource_ptr[i]);
+        pthread_join(BufferTid[i],&resource_ptr[i]);
 
         fprintf(stderr,"address of buffer is %p and values are %d and %d\n",
         (int*)resource_ptr[i],*(int*)resource_ptr[i],*((int*)resource_ptr[i]+1));
+        
+        release_buffer(resource_ptr[i]);
+        if(sem_getvalue(&res_sema,&sem_value)==0)
+        {
+            printf("semaphore value is %d\n",sem_value);
+        }
+        else{
+            perror("error while getting sem value\n:");
+        }
 
     }
-    
-    for (size_t i = 0; i < RESOURCE_COUNT; i++)
-    {
-        sem_destroy(&res_sema[i]);
-    }
-    
+
+    sem_destroy(&res_sema);
     
 
-    
     
     return 0;
 }
