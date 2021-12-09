17efdd6feab19f85376902f8712e434ec381a92c
murthuja <murthuja1708@gmail.com>
2021-12-09 14:01:50 +0530

    receiving and sending data in non blocking mode

--- processes/message_queues/receiver.c
+++ processes/message_queues/receiver.c
@@ -4,11 +4,14 @@
 #include <sys/stat.h> /* For mode constants */
 #include <fcntl.h>  /* For O_* constants */
 #include<stdio.h>
+#include<errno.h>
+#include<string.h>
+
 
 #define MAX_MSG_SIZE  10
-#define MAX_NO_MSGS  5
+#define MAX_NO_MSGS  6
 
-char msg[5][10];
+char msg[10];
 int msg_prio;
 
 mqd_t msg_que;
@@ -19,19 +22,50 @@ int main()
 
     msg_attr.mq_msgsize=(long)MAX_MSG_SIZE;
     msg_attr.mq_maxmsg=(long)MAX_NO_MSGS;
-    msg_attr.mq_flags=0;
+    msg_attr.mq_flags=O_NONBLOCK;
     msg_attr.mq_curmsgs=0;
 
     msg_que = mq_open("/msg_queue",(O_CREAT|O_RDONLY),(S_IRUSR|S_IWUSR),&msg_attr);
 
+    
 
-    for (size_t i = 0; i < MAX_NO_MSGS; i++)
+    if(mq_getattr(msg_que,&msg_attr)==-1)
     {
-        mq_receive(msg_que,msg[i],MAX_MSG_SIZE,&msg_prio);
-        fprintf(stderr,"msg read :%s\n",msg[i]);
+        fprintf(stderr,"couldnt set attributes\n");
     }
 
-    
+    size_t count=msg_attr.mq_curmsgs;
+
+    if(count==0)
+    {
+        fprintf(stderr,"no new messages\n");
+    }
+    else{
+        while (count > 0)
+        {
+            if(mq_receive(msg_que,msg,MAX_MSG_SIZE,&msg_prio)==-1)
+            {
+                switch (errno)
+                {
+                case EAGAIN:
+                    perror("error");
+                    break;
+                case EINTR:
+                    perror("error");
+                    break;
+                
+                default:
+                    break;
+                }
+                break;
+            }
+            else{
+                fprintf(stderr,"msg read :%s\n",msg);
+                memset(msg,'\0',MAX_MSG_SIZE);
+                count--;
+            }
+        }
+    }
 
     mq_close(msg_que);
 
--- processes/message_queues/sender.c
+++ processes/message_queues/sender.c
@@ -4,15 +4,21 @@
 #include <sys/stat.h> /* For mode constants */
 #include <fcntl.h>  /* For O_* constants */
 #include<string.h>
+#include<errno.h>
+#include<stdio.h>
+#include<unistd.h>
 
 #define MAX_MSG_SIZE  10
-#define MAX_NO_MSGS  5
+#define MAX_NO_MSGS  6
 
-char* dummy_messages[MAX_NO_MSGS]={"hello",
+const char* dummy_messages[MAX_NO_MSGS+1]={"hello",
                         "bye",
                         "cdac",
                         "desd",
-                        "hyderabad"};
+                        "hyderabad",
+                        NULL};
+const char **temp=dummy_messages;
+
 
 char* bye_string="bye\n";
 
@@ -23,16 +29,39 @@ int main()
     struct mq_attr msg_attr;
     msg_attr.mq_msgsize=(long)MAX_MSG_SIZE;
     msg_attr.mq_maxmsg=(long)MAX_NO_MSGS;
-    msg_attr.mq_flags=0;
+    msg_attr.mq_flags=O_NONBLOCK;
     msg_attr.mq_curmsgs=0;
 
     msg_que = mq_open("/msg_queue",(O_CREAT|O_RDWR),(S_IRUSR|S_IWUSR),&msg_attr);
 
-    for (size_t i = 0; i < MAX_NO_MSGS; i++)
+    size_t count=0;
+    while(*temp!=NULL)
     {
-        mq_send(msg_que,dummy_messages[i],strlen(dummy_messages[i]),0);
+        if(mq_send(msg_que,*temp,strlen(*temp),0)==-1)
+        {
+            switch (errno)
+            {
+            case EAGAIN:
+                perror("wait for 5 seconds and send");
+                break;
+            case EINTR:
+                perror("error");
+                break;
+            
+            default:
+                break;
+            }
+            break;
+        }
+        else{
+            count++;
+            temp++;
+        }
     }
+    fprintf(stderr,"total %ld messages we sent\n",count);
 
+
+    
     mq_close(msg_que);
 
     return 0;
