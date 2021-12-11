ccc1b8170a0df35f0a60e4efa96ea47f926df2af
murthuja <murthuja1708@gmail.com>
2021-12-11 21:03:49 +0530

    added two semaphores

--- processes/shared_memories/consumer1.c
+++ processes/shared_memories/consumer1.c
@@ -6,43 +6,78 @@
  #include <unistd.h>
 #include <sys/types.h>
 #include<stdio.h>
-
+#include<string.h>
 #include<semaphore.h>
+#include<stdlib.h>
 
 #define SHARED_MEMORY_SIZE  20
 
-void* addr;
+char* addr;
 
 int main()
 {
-    int shm_fd=shm_open("/imp_data",O_RDONLY,S_IRUSR|S_IWUSR);
+    int shm_fd=shm_open("/imp_data",O_RDWR|O_CREAT,S_IRUSR|S_IWUSR);
+
+    if(shm_fd == -1)
+    {
+        perror("shm open");
+        exit(EXIT_FAILURE);
+    }
 
     ftruncate(shm_fd,SHARED_MEMORY_SIZE);
 
-    addr=mmap(NULL,SHARED_MEMORY_SIZE,PROT_WRITE|PROT_READ,MAP_SHARED,shm_fd,0);
+    addr=(char*)mmap(NULL,SHARED_MEMORY_SIZE,PROT_WRITE|PROT_READ,MAP_SHARED,shm_fd,0);
 
-    sem_t* shm_sema=sem_open("/imp_data_sema",O_CREAT|O_RDWR);
+    if(addr==NULL)
+    {
+        perror("mmap:");
+    }
 
-    if(shm_sema == NULL)
+    sem_unlink("/sema_cons");
+    sem_unlink("/sema_prod");
+
+    sem_t* sem_cons=sem_open("/sema_cons",O_CREAT|O_RDWR,0660,0);
+
+    if(sem_cons == SEM_FAILED)
     {
         perror("error creating sema\n");
         return 0;
     }
 
-    sem_init(shm_sema,1,2);
+    //sem_init(sem_cons,1,0);
+
+
+    sem_t* sem_prod=sem_open("/sema_prod",O_CREAT|O_RDWR,0660,1);
+
+    
+
+    if(sem_prod == SEM_FAILED)
+    {
+        perror("error creating sema\n");
+        return 0;
+    }
+
+    //sem_init(sem_prod,1,1);
 
     int sem_value;
-    
+    char ch;
+    char* temp;
     while(1)
     {
-        fprintf(stderr,"waiting for semaphore\n");
-        sem_getvalue(shm_sema,&sem_value);
-        fprintf(stderr,"sem value is %d\n",sem_value);
-        sem_wait(shm_sema);
-        printf("%s",(char*)addr);
-        sem_post(shm_sema);
+        sem_wait(sem_cons);
+        printf("----%s-----\n",addr);
+        if(strncmp(addr,"quit",4)==0)
+        {
+            break;
+        }
+        sleep(2);
+        //sem_post(sem_prod);
 
     }
+
+    sem_close(sem_cons);
+    sem_close(sem_prod);
+    
     
 
     return 0;
--- processes/shared_memories/producer1.c
+++ processes/shared_memories/producer1.c
@@ -7,41 +7,74 @@
 #include <sys/types.h>
 #include<stdio.h>
 #include<string.h>
-
+#include<pthread.h>
 #include<semaphore.h>
-
+#include<stdlib.h>
 
 
 #define SHARED_MEMORY_SIZE  20
 
-void* addr;
+char* addr;
 
 
 int main()
 {
     int shm_fd=shm_open("/imp_data",O_RDWR|O_CREAT,S_IRUSR|S_IWUSR);
+    if(shm_fd == -1)
+    {
+        perror("shm open");
+        exit(EXIT_FAILURE);
+    }
 
     ftruncate(shm_fd,SHARED_MEMORY_SIZE);
     
-    addr=mmap(NULL,SHARED_MEMORY_SIZE,PROT_WRITE|PROT_READ,MAP_SHARED,shm_fd,0);
-    
-    char res[SHARED_MEMORY_SIZE]="murthu";
+    addr=(char*)mmap(NULL,SHARED_MEMORY_SIZE,PROT_WRITE|PROT_READ,MAP_SHARED,shm_fd,0);
 
-    sem_t* shm_sema=sem_open("/imp_data_sema",O_CREAT|O_RDWR);
+    sem_t* sem_prod=sem_open("/sema_prod",O_RDWR,0660,0);
 
-    sem_init(shm_sema,1,2);
-    
-    while (1)
+    if(sem_prod == SEM_FAILED)
     {
-        fprintf(stderr,"waiting for semaphore\n");
-        sem_wait(shm_sema);
-        fprintf(stderr,"acquired semaphore\n");
-        memcpy(addr,res,sizeof(SHARED_MEMORY_SIZE));
-        sem_post(shm_sema);
-
-        sleep(2);
+        perror("error creating sema\n");
+        return 0;
     }
 
+    //sem_init(sem_prod,1,0);
+
+    
+
+    sem_t* sem_cons=sem_open("/sema_cons",O_RDWR,0660,1);
+
+       if(sem_cons == SEM_FAILED)
+        {
+            perror("error creating sema\n");
+            return 0;
+        }
+
+    //sem_init(sem_cons,1,0);
+
+    char res[SHARED_MEMORY_SIZE][10]={"hello","man","murthu","jilani","tabbu","tannu","waste","quit"};
+    size_t len;
+    //while(1)
+    //{
+        for (size_t i = 0; i < 8; i++)
+        {
+            fprintf(stderr,"waiting for semaphore\n");
+            sem_wait(sem_prod);
+            len=strlen(res[i]);
+            addr[len]='\0';
+            fprintf(stderr,"acquired semaphore\n");
+            strncpy(addr,res[i],strlen(res[i])+1);
+            sem_post(sem_cons);
+        }
+
+    //}
+    
+
+    //sem_init(shm_sema,1,2);
+    
+    
+    sem_close(sem_cons);
+    sem_close(sem_prod);
     
     return 0;
 
